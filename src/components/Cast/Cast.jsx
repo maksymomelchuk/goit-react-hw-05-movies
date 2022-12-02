a36a85a9cca5dad19b00abfa8325669d902abb52
maksymomelchuk <omelchukmaksym@gmail.com>
2022-12-02 16:50:30 +0200

    commit

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,5 +1,5 @@
 import { fetchActors } from 'services/api';
-import { Suspense, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useState } from 'react';
 import { StyledActorsList, StyledActorCard } from './Cast.styled';
