8ac7ef140a017a6fe00a7d3e847e8e52389439cf
maksymomelchuk <omelchukmaksym@gmail.com>
2022-12-02 17:05:38 +0200

    before sending to tutor

--- src/components/Movies/MoviesList.jsx
+++ src/components/Movies/MoviesList.jsx
@@ -16,7 +16,11 @@ const MoviesList = ({ data }) => {
           <Link to={`/movies/${id}`} key={id} state={{ from: location }}>
             <StyledMoviesElement>
               <img
-                src={`https://image.tmdb.org/t/p/w200${poster_path}`}
+                src={
+                  poster_path
+                    ? `https://image.tmdb.org/t/p/w200${poster_path}`
+                    : `https://upload.wikimedia.org/wikipedia/ru/thumb/1/17/%D0%91%D1%80%D0%B8%D0%BB%D0%BB%D0%B8%D0%B0%D0%BD%D1%82%D0%BE%D0%B2%D0%B0%D1%8F_%D1%80%D1%83%D0%BA%D0%B0_Poster.jpg/800px-%D0%91%D1%80%D0%B8%D0%BB%D0%BB%D0%B8%D0%B0%D0%BD%D1%82%D0%BE%D0%B2%D0%B0%D1%8F_%D1%80%D1%83%D0%BA%D0%B0_Poster.jpg`
+                }
                 alt=""
               />
               {/* <p>{title ? title : name}</p> */}
