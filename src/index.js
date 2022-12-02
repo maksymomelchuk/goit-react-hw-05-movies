5f72a32a75afaa214339638981a40767a899b74d
maksymomelchuk <omelchukmaksym@gmail.com>
2022-12-02 11:06:17 +0200

    del basename

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode basename="/goit-react-hw-05-movies">
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
